aab2263ada62012cfaddae9b2fc3b8b95f521def
Chanakan Mungtin <chanakan5591@gmail.com>
2020-07-09 04:59:26 +0700

    Init Commit
    First Commit for Pogger bot.

---
+++ Pogger/Global.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+public class Global
+{
+	public static string Token { get; set; }
+	
+	public static void ReadConfig()
+    {
+		dynamic data = JsonConvert.DeserializeObject(File.ReadAllText($"{Environment.CurrentDirectory}/data.json"));
+		Token = data.Token;
+    }
+	
+}
+
---
+++ Pogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Pogger
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            Global.ReadConfig();
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, Global.Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
