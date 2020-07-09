0f01e2a73013dd8756f7d23511d31fb28e6d55e1
Chanakan Mungtin <chanakan5591@gmail.com>
2020-07-09 20:00:52 +0700

    Added Status to the bot cuz why not :smile:
    Created CommandHandler File for status. Commands will later be add.

--- Pogger/Global.cs
+++ Pogger/Global.cs
@@ -1,15 +1,25 @@
 ﻿using Newtonsoft.Json;
 using System;
+using BetterCommandService;
 using System.IO;
+using Discord.WebSocket;
 
 public class Global
 {
 	public static string Token { get; set; }
-	
+	public static char Prefix { get; set; }
+
+	public static DiscordSocketClient Client { get; set; }
+
+	public static CustomCommandService Command { get; set; }
+
+	public static String Status { get; set; }
 	public static void ReadConfig()
     {
 		dynamic data = JsonConvert.DeserializeObject(File.ReadAllText($"{Environment.CurrentDirectory}/data.json"));
 		Token = data.Token;
+		Prefix = data.Prefix;
+		Status = data.Status;
     }
 	
 }
--- Pogger/Program.cs
+++ Pogger/Program.cs
@@ -1,4 +1,5 @@
-﻿using Discord;
+﻿using BetterCommandService;
+using Discord;
 using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
@@ -8,24 +9,47 @@ namespace Pogger
     public class Program
     {
         private DiscordSocketClient _client;
+        private CommandHandler _handler; 
+        private HandlerService handlerService;
+
         public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
             Global.ReadConfig();
-            _client = new DiscordSocketClient();
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug,
+                AlwaysDownloadUsers = true,
+            }); 
             _client.Log += Log;
 
             await _client.LoginAsync(TokenType.Bot, Global.Token);
             await _client.StartAsync();
 
+            Global.Client = _client;
+
+            CustomCommandService _commands = new CustomCommandService(new Settings()
+            {
+                DefaultPrefix = Global.Prefix
+            });
+
+            handlerService = new HandlerService(_client);
+            _handler = new CommandHandler(_client, _commands, handlerService);
+
             await Task.Delay(-1);
+
         }
 
-        private Task Log(LogMessage msg)
+        private async Task Log(LogMessage msg)
         {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
+            if (msg.Message == null)
+                return;
+            if (!msg.Message.StartsWith("Received Dispatch"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[Svt: {msg.Severity} Src: {msg.Source} Ex: {msg.Exception}] - " + msg.Message);
+            }
         }
     }
 }
