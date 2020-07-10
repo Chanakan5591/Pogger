2dc9c87445248bd21cb4b3c32dc942cb025097f4
Chanakan Mungtin <chanakan5591@ChanaDeveloper-Linux.localdomain>
2020-07-10 08:31:07 +0700

    Add ping commands and message log

--- Pogger/Global.cs
+++ Pogger/Global.cs
@@ -1,17 +1,14 @@
 ﻿using Newtonsoft.Json;
 using System;
-using BetterCommandService;
 using System.IO;
 using Discord.WebSocket;
 
 public class Global
 {
 	public static string Token { get; set; }
-	public static char Prefix { get; set; }
+	public static string Prefix { get; set; }
 
 	public static DiscordSocketClient Client { get; set; }
-
-	public static CustomCommandService Command { get; set; }
 
 	public static String Status { get; set; }
 	public static void ReadConfig()
--- Pogger/Program.cs
+++ Pogger/Program.cs
@@ -1,54 +1,96 @@
-﻿using BetterCommandService;
-using Discord;
-using Discord.WebSocket;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Pogger
 {
-    public class Program
+    class Program
     {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
         private DiscordSocketClient _client;
-        private CommandHandler _handler; 
-        private HandlerService handlerService;
+        private CommandService _commands;
+        private IServiceProvider _services;
 
-        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-
-        public async Task MainAsync()
+        public async Task RunBotAsync()
         {
             Global.ReadConfig();
             _client = new DiscordSocketClient(new DiscordSocketConfig
             {
                 LogLevel = LogSeverity.Debug,
                 AlwaysDownloadUsers = true,
-            }); 
-            _client.Log += Log;
+            });
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            _client.Log += _client_Log;
+            
+            _client.SetGameAsync(Global.Status, null, ActivityType.Playing);
+
+            _client.SetStatusAsync(UserStatus.Online);
+            
+            _client.MessageReceived += LogMessage;
+
+            await RegisterCommandsAsync();
 
             await _client.LoginAsync(TokenType.Bot, Global.Token);
+
             await _client.StartAsync();
-
-            Global.Client = _client;
-
-            CustomCommandService _commands = new CustomCommandService(new Settings()
-            {
-                DefaultPrefix = Global.Prefix
-            });
-
-            handlerService = new HandlerService(_client);
-            _handler = new CommandHandler(_client, _commands, handlerService);
 
             await Task.Delay(-1);
 
         }
 
-        private async Task Log(LogMessage msg)
+        private async Task _client_Log(LogMessage msg)
         {
+           
             if (msg.Message == null)
                 return;
             if (!msg.Message.StartsWith("Received Dispatch"))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"[Svt: {msg.Severity} Src: {msg.Source} Ex: {msg.Exception}] - " + msg.Message);
+            }
+        }
+        
+        private async Task LogMessage(SocketMessage arg)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            var msg = arg as SocketUserMessage;
+            if(msg.Author.IsBot) return;
+            Console.Write("Message from " + arg.Author + ": ", ConsoleColor.Magenta);
+            Console.WriteLine(msg);
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix(Global.Prefix, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
             }
         }
     }
